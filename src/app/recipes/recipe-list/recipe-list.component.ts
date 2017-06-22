11f7ad4052cc91bdf13c40abe3b5ba3194b7f7d5
ilashchenko <ilashchenko@youa.vn.ua>
2017-06-22 17:47:26 +0300

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'test desc', 'https://c1.staticflickr.com/3/2547/3862672238_30d378e7d6.jpg'),
+    new Recipe('A test recipe', 'test desc', 'https://c1.staticflickr.com/3/2547/3862672238_30d378e7d6.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
